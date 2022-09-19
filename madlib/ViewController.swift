3ba77a0ee65058fa4b6ed6bec4aff6b15eacc431
Julian De La Torre <jujudlt3@gmail.com>
2022-09-19 10:02:00 -0700

    added new screen

--- madlib/ViewController.swift
+++ madlib/ViewController.swift
@@ -9,6 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var Sentence: UILabel!
+    
+    
+    @IBOutlet weak var input: UITextField!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nvc = segue.destination as! MadLibViewController
+    }
+
+    @IBAction func button(_ sender: UIButton) {
+        performSegue(withIdentifier: "mySegue", sender: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
